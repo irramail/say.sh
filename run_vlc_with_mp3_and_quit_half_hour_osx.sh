2fee80c0723a3554cab3780f63c7e2ba64512107
irramail <irramail@gmail.com>
2020-06-04 03:25:42 +0700

    Update run_vlc_with_mp3_and_quit_half_hour_osx.sh

--- run_vlc_with_mp3_and_quit_half_hour_osx.sh
+++ run_vlc_with_mp3_and_quit_half_hour_osx.sh
@@ -1,1 +1,1 @@
-while :; do; scp p:output.mp3 ./ ; /Applications/VLC.app/Contents/MacOS/VLC Downloads/testmpga/output.mp3 vlc://quit ; sleep 1800; done
+while :; do; scp p:output.mp3 ./ ; /Applications/VLC.app/Contents/MacOS/VLC ./output.mp3 vlc://quit ; sleep 1800; done
